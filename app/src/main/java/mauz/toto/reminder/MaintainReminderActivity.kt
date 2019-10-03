35b283ce50f32cb2b7217bea2bf828e0203211b5
Toto <torsten.quast1@gmail.com>
2019-10-03 21:03:03 +0200

    Update Reminder UI every 60 s
    We show only minutes, so every 60 s should be enough
    
    Fixes #18

--- app/src/main/java/mauz/toto/reminder/MaintainReminderActivity.kt
+++ app/src/main/java/mauz/toto/reminder/MaintainReminderActivity.kt
@@ -3,15 +3,18 @@ package mauz.toto.reminder
 import android.app.PendingIntent
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
+
 class MaintainReminderActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
+    private var handler = Handler()
 
     companion object {
         const val TOKEN = "MaintainReminder"
@@ -21,6 +24,7 @@ class MaintainReminderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maintain_reminder)
+        handler.post(runnableCode)
 
         viewManager = LinearLayoutManager(this)
         viewAdapter = ReminderAdapter(INTENTS)
@@ -46,5 +50,13 @@ class MaintainReminderActivity : AppCompatActivity() {
         val position = INTENTS.indexOf(intent)
         INTENTS.removeAt(position)
         viewAdapter.notifyDataSetChanged()
+    }
+
+    private val runnableCode = object : Runnable {
+        override fun run() {
+            Log.d(TOKEN, "Update UI tick")
+            viewAdapter.notifyDataSetChanged()
+            handler.postDelayed(this, 60000)
+        }
     }
 }

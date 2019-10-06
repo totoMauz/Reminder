033f2cec6c4421ad426c3c5ddd5eeb0fca17071f
Toto <torsten.quast1@gmail.com>
2019-10-06 21:22:00 +0200

    Refactoring

--- app/src/main/java/mauz/toto/reminder/MaintainReminderActivity.kt
+++ app/src/main/java/mauz/toto/reminder/MaintainReminderActivity.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
-import android.view.ActionMode
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
